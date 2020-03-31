0fe62b17fcc8177f8746da0822d4e20475a02b27
amanaksu <amanaksu@gmail.com>
2020-03-31 20:45:16 +0900

    Coding the Action part 2 - Thumbnail

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -16,7 +16,8 @@ const CategoryOptions = [
     {value: 0, label: "Film & Animation"},
     {value: 1, label: "Autos & Vehicles"},
     {value: 2, label: "Music"},
-    {value: 3, label: "Pets & Animals"}
+    {value: 3, label: "Pets & Animals"},
+    {value: 4, label: "News"}
 ];
 
 function VideoUploadPage() {
@@ -24,6 +25,9 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState("");
     const [Private, setPrivate] = useState(0);  // Private : 0, Public : 1
     const [Category, setCategory] = useState("Film & Animation");
+    const [FilePath, setFilePath] = useState("");
+    const [Duration, setDuration] = useState("");
+    const [ThumbnailPath, setThumbnailPath] = useState("");
 
     const onTitleChange = (event) => {
         setVedioTitle(event.currentTarget.value);
@@ -37,6 +41,7 @@ function VideoUploadPage() {
     const onCategoryChange = (event) => {
         setCategory(event.currentTarget.value);
     };
+    
     const onDrop = (files) => {
         let formData = new FormData;
         const config = {
@@ -46,7 +51,22 @@ function VideoUploadPage() {
 
         axios.post("/api/video/uploadfiles", formData, config).then(response => {
             if(response.data.success) {
-                console.log(response.data);
+
+                let thumbnail_options = {
+                    url: response.data.url,
+                    fileName: response.data.fileName
+                };
+
+                setFilePath(response.data.url);
+
+                axios.post("/api/video/thumbnail", thumbnail_options).then(response => {
+                    if(response.data.success) {
+                        setDuration(response.data.fileDuration);
+                        setThumbnailPath(response.data.url);
+                    } else {
+                        alert("Failed Make a Thumbnail.");
+                    }
+                });
             } else {
                 alert("Failed Upload Video");
             }
@@ -63,7 +83,7 @@ function VideoUploadPage() {
             <Form onSubmit>
                 <div style={{ display:"flex", justifyContent:"space-between" }}>
                     {/* Drop Zone */}
-                    <Dropzone onDrop={onDrop} multiple={false} maxSize={1048576}> 
+                    <Dropzone onDrop={onDrop} multiple={false} maxSize={300000000}> 
                         {({ getRootProps, getInputProps }) => (
                             <div style={{ width:"300px", height:"240px", border:"1px solid lightgray", display:"flex", alignItems:"center", justifyContent:"center"}} {...getRootProps()}>
                                 <input {...getInputProps()}></input>
@@ -73,9 +93,11 @@ function VideoUploadPage() {
                     </Dropzone>
 
                     {/* Thumbnail */}
-                    <div>
-                        <img src alt />
-                    </div>
+                    {ThumbnailPath &&
+                        <div>
+                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail"></img>
+                        </div> 
+                    }
                 </div>
                 <br />
                 <br />
--- server/routes/video.js
+++ server/routes/video.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
+const ffmpeg = require("fluent-ffmpeg");
 
 
 
@@ -44,11 +45,48 @@ router.post("/uploadfiles", (req, res) => {
         } else {
             return res.json({
                 success: true,
-                url: res.req.file.path,
+                url: res.req.file.path, 
                 fileName: res.req.file.filename
             });
         }
     })
+});
+
+router.post("/thumbnail", (req, res) => {
+    // 썸네일을 생성하고 비디오 정보를 가져오기 
+
+    let fileDuration = "";                  // 비디오 정보 - RunningTime
+    let filePath = "";                      // 저장된 썸네일 파일명
+
+    // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.url, function(error, metadata) {
+        fileDuration = metadata.format.duration;
+    });
+
+    // 썸네일 생성
+    ffmpeg(req.body.url)
+    .on("filenames", function (filenames) {             // 썸네일 파일 저장 위치 
+        filePath = "uploads/thumbnails/" + filenames[0]
+    })
+    .on("end", function() {                             // 썸네일 생성시
+        return res.json({
+            success: true,
+            url: filePath, 
+            fileDuration: fileDuration
+        });
+    })
+    .on("error", function(error) {                      // 썸네일 생성 실패시
+        return res.json({
+            success: false,
+            error
+        });
+    })
+    .screenshot({
+        count: 3,                           // 썸네일 개수
+        folder: "uploads/thumbnails",       // 썸네일 저장 폴더
+        size: "320x240",                    // 썸네일 크기
+        filename: "thumbnail-%b.png"        // 썸네일 파일명 (%b : 원본 파일명에서 확장자를 제외한 파일명)
+    });
 });
 
 module.exports = router;
